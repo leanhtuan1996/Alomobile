e339412038e323374178dc1f18deebe11109034a
Le Anh Tuan <leanhtuan110596@gmail.com>
2018-04-02 16:50:38 +0700

    add new function for cart

--- public/js/myFunc.js
+++ public/js/myFunc.js
@@ -32,6 +32,93 @@ function logout() {
     })
 }
 
+function addToCart(item) {      
+    if (!item.id) { return false }
+    if (!item.color) { return false }
+    if (!item.quantity) { return false }
+
+    try {
+        item.quantity = Number.parseInt(item.quantity);
+        item.id = String(item.id);
+        item.color = String(item.color);            
+    } catch (error) {
+        return null
+    }       
+
+    if (!localStorage) {
+        return null
+    }
+    
+    if (!localStorage.cart) {
+        return saveToStorage([item])
+    } else {
+        var cart;
+        try {
+            cart = JSON.parse(localStorage.cart);
+        } catch (error) {
+            return null
+        }
+        
+        if (!cart) { return null }
+        var products = cart.products;
+        if (!Array.isArray(products)) { return null }            
+
+        var idx = findItemExist(products, item.id, item.color);
+        if (idx > -1) {          
+            products[idx].quantity += item.quantity
+            return saveToStorage(products)
+        } else {
+            products.push(item);
+            return saveToStorage(products);
+        }
+    }
+}
+
+function saveToStorage(products) {
+    try {
+        localStorage.cart = JSON.stringify({ products: products })
+        return products
+    } catch (error) {
+        return null
+    }        
+}
+
+function findItemExist(products, id, color) {  
+    return products.findIndex(element => {           
+        return element.id == id && element.color == color
+    });
+}
+
+function checkRemainingQuantity(id, color, remainingQuantity, orderingQuantity) {
+    
+}
+
+function updateFromCart(id, color, quantity) {
+    if (!localStorage.cart || localStorage.cart.length == 0) { return }
+    var idx = localStorage.cart.findIndex(element => {
+        return element.id = id && element.color == color
+    });
+
+    if (idx && idx > -1) {
+        localStorage.cart[idx].quantity = quantity
+    }
+}
+
+function removeFromCart(id) {
+    if (!localStorage.cart || localStorage.cart.length == 0) { return }
+
+    if (Array.isArray(localStorage.cart)) {
+        var idx = localStorage.cart.findIndex(element => {
+            return element.id == id
+        });
+        if (idx && idx > - 1) { 
+            localStorage.cart.slice(idx, 1);
+        } 
+    } else {
+        return
+    }
+}
+
 const numberWithCommas = (x) => {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
